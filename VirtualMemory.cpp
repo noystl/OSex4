08576f67ef04df2dec42d9a1c2556106a9b84563
noys <noystll@gmail.com>
2019-05-21 15:43:45 -0400

    Initial commit

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,6 +1,25 @@
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
+//----------------------------Private Function
+
+uint64_t translate(uint64_t toTrans){
+    uint64_t doNotEvict[TABLES_DEPTH];  //TODO: Verify that this is the right array length
+    uint64_t phisicalAddr;
+    // TODO: Do.
+    return phisicalAddr;
+}
+
+uint64_t findFrame(const uint64_t& doNotDelete){ // Gets an array of frames it shouldn't evict.
+    uint64_t phisicalAddr;
+    // Max cyclic score
+    // Max frame with max cyclic score
+    // Max frame index
+    // TODO: Do.
+    return phisicalAddr;
+}
+
+//----------------------------- Public Functions
 void clearTable(uint64_t frameIndex) {
     for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
         PMwrite(frameIndex * PAGE_SIZE + i, 0);
@@ -10,7 +29,6 @@ void clearTable(uint64_t frameIndex) {
 void VMinitialize() {
     clearTable(0);
 }
-
 
 int VMread(uint64_t virtualAddress, word_t* value) {
     return 1;
