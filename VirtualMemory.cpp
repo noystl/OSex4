f0872aeccb60ec03e3a6511c4ecd06249c574dce
baraloni <bar11aloni@gmail.com>
2019-06-03 13:14:40 +0300

    findFrame- in process

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -1,8 +1,14 @@
 #include <cmath>
+#include <iterator>
 #include "VirtualMemory.h"
 #include "PhysicalMemory.h"
 
 //----------------------------Private Function
+
+#define MAX(X, Y)  ((X) > (Y) ? (X) : (Y))
+#define MIN(X, Y)  ((X) < (Y) ? (X) : (Y))
+#define ABS(X)  ((X) >= 0 ? (X) : (-X))
+
 
 void clearTable(uint64_t frameIndex) {
     for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
@@ -10,13 +16,102 @@ void clearTable(uint64_t frameIndex) {
     }
 }
 
-uint64_t findFrame(const uint64_t (&doNotDelete)[TABLES_DEPTH]){ // Gets an array of frames it shouldn't evict.
-    uint64_t phisicalAddr;
-    // Max cyclic score
-    // Max frame with max cyclic score
-    // Max frame index
-    // TODO: Do.
-    return phisicalAddr;
+
+bool findFrameHelper(word_t currFrameIdx, unsigned int hierLevel, word_t currParentIdx, uint64_t pathToFrame,
+                     word_t &emptyFrameIdx, word_t &unusedFrameIdx, word_t &frameIdxWithMaxCyclicScore,
+                     uint64_t &maxCyclicScore, word_t &parentIdx,
+                     const uint64_t &pageIndex, const uint64_t (&doNotDelete)[TABLES_DEPTH])
+{
+    bool isFirstFulfilled = true;
+    word_t tableEntry;
+
+    // Check if 1st criteria is fulfilled:
+    for (unsigned int i = 0; i < PAGE_SIZE; ++i) //stop condition: empty frame. TODO: check bounds
+    {
+        PMread(currFrameIdx * PAGE_SIZE + i, &tableEntry);
+        if (tableEntry != 0)
+        {
+            isFirstFulfilled = false;
+            unusedFrameIdx = MAX(unusedFrameIdx,tableEntry); //maintain 2nd criteria. TODO: what if we're in a leaf?
+        }
+    }
+
+    // Check that it's not a "false alarm"- the frame belongs to path:
+    if (isFirstFulfilled)
+    {
+        if (std::find(std::begin(doNotDelete), std::end(doNotDelete), currFrameIdx) == std::end(doNotDelete))
+        {
+            //prepare for successful return on 1st criteria:
+            parentIdx = currParentIdx;
+            emptyFrameIdx = currFrameIdx;
+        }
+        else{
+            isFirstFulfilled = false;
+        }
+    }
+
+    // Respond accordingly:
+    if (!isFirstFulfilled)
+    {
+        // stop condition: leaf.
+        if (hierLevel == TABLES_DEPTH) //TODO: check bounds
+        {
+            uint64_t absDist = ABS(pageIdx - pathToFrame);
+            uint64_t pageDist = MIN(NUM_PAGES - absDist, absDist);
+            if(pageDist > maxCyclicScore)
+            {
+                maxCyclicScore = pageDist; //maintain 3rd criteria
+
+                //prepare for successful return on 3rd criteria:
+                frameIdxWithMaxCyclicScore = currFrameIdx;
+                parentIdx = currParentIdx;
+            }
+        }
+        else
+        {
+            //recursion:
+            for (unsigned int i = 0; i < PAGE_SIZE; ++i) //TODO: check bounds
+            {
+                PMread(currFrameIdx * PAGE_SIZE + i, &tableEntry);
+                if (tableEntry != 0)
+                {
+                    return findFrameHelper(tableEntry, hierLevel + 1, currFrameIdx, pathToFrame, //TODO: operate func on pathToFrame
+                                    emptyFrameIdx, unusedFrameIdx, frameIdxWithMaxCyclicScore,
+                                    maxCyclicScore, parentIdx, pageIndex, doNotDelete);
+                }
+            }
+        }
+    }
+
+    //return indicator suggesting to which return value to address:
+    return isFirstFulfilled;
+}
+
+
+word_t findFrame(const uint64_t &pageIndex, const uint64_t &offset,
+                 const uint64_t (&doNotDelete)[TABLES_DEPTH])
+{
+    //will contain the frames that answer thw 1st, 2nd and 3rd criteria accordingly:
+    word_t emptyFrameIdx = 0, unusedFrameIdx = 0, frameIdxWithMaxCyclicScore = 0;
+    uint64_t maxCyclicScore = 0;
+    word_t parentIdx = 0;
+
+    bool isFirstFulfilled = findFrameHelper(0, 0, NULL, NULL, emptyFrameIdx, unusedFrameIdx,
+                                            frameIdxWithMaxCyclicScore, maxCyclicScore,
+                                            parentIdx, pageIndex, doNotDelete);
+    if (isFirstFulfilled)
+    {
+        //dereference from parent:
+        //TODO
+        return emptyFrameIdx;
+    }
+    if (unusedFrameIdx + 1 < NUM_FRAMES)
+    {
+        return unusedFrameIdx + 1;
+    }
+    //dereference from parent:
+    //TODO
+    return frameIdxWithMaxCyclicScore;
 }
 
 
@@ -117,7 +212,7 @@ uint64_t translate(uint64_t toTrans){
         // If the requested table is not in memory (Page Fault):
         if(nextFrameIndex == 0){
             // Find and init a new table:
-            nextFrameIndex = (word_t)findFrame(doNotEvict);
+            nextFrameIndex = findFrame(pageIndex, offset, doNotEvict);
             clearTable(nextFrameIndex);
             PMwrite(currFrameIndex * PAGE_SIZE + searchRows[i], nextFrameIndex);
 
@@ -133,7 +228,7 @@ uint64_t translate(uint64_t toTrans){
 
     // If the requested page is not in memory (Page Fault):
     if(nextFrameIndex == 0){
-        nextFrameIndex = (word_t)findFrame(doNotEvict);
+        nextFrameIndex = findFrame(pageIndex, offset, doNotEvict);
         PMrestore(nextFrameIndex, pageIndex);
         PMwrite(currFrameIndex * PAGE_SIZE + searchRows[TABLES_DEPTH - 1], nextFrameIndex);
     }
@@ -155,7 +250,7 @@ int VMread(uint64_t virtualAddress, word_t* value) {
 
 
 int VMwrite(uint64_t virtualAddress, word_t value) {
-    uint64_t phisicalAddress = translate(virtualAddress);
-    PMwrite(phisicalAddress, value);
+    uint64_t physicalAddress = translate(virtualAddress);
+    PMwrite(physicalAddress, value);
     return 1;
 }
